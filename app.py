b1a62710b7425cb169ea6c02db7b92413baa2787
Andrew <cyberandrew30@yahoo.com>
2021-09-28 00:24:56 -0400

    intial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import pytesseract
+from PIL import Image
+import os
+from flask import Flask, request, send_file, jsonify
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+ROOT_DIR = os.path.abspath(os.curdir)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        files = request.files
+        file = files.get('image')
+        file.save(secure_filename(file.filename))
+        im = Image.open(f'./{file.filename}').convert('RGB')
+        im.save(f'./{file.filename}.webp', 'webp')
+
+        @app.after_request
+        def delete(response):
+            if os.path.exists(f'{file.filename}.webp'):
+                os.remove(f'./{file.filename}.webp')
+                os.remove(f'./{file.filename}')
+            return response
+
+        return send_file(f'./{file.filename}.webp', mimetype='image/webp')
+
+
+@app.route('/gettext', methods=['POST'])
+def get_text():
+    if request.method == 'POST':
+        files = request.files
+        file = files.get('image')
+        file.save(secure_filename(file.filename))
+        text = pytesseract.image_to_string(Image.open(f'{file.filename}'))
+        if os.path.exists(f'{file.filename}'):
+            os.remove(f'./{file.filename}')
+        return jsonify(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
